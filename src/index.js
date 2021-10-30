9387a5f5071224a3239429d0722104efacbd88c4
freevedo <dgui498@gmail.com>
2021-10-30 12:34:01 +0200

    set app as the first component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+
 
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <Counters />
+  <App />
   ,
   document.getElementById('root')
 );
